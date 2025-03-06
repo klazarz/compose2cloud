c15547f8131d4806f5750d82282c2f36da244299
Kevin Lazarz <kevin.lazarz@oracle.com>
2025-03-06 14:42:00 +0100

    ddd

--- inst.sh
+++ inst.sh
@@ -2,7 +2,6 @@
 
 source init/variable.sh
 
-#######
 #######    S T A R T      S C R I P T    ######
 #######   (this is for Oracle Linux 9)   ######
 
@@ -25,12 +24,7 @@ sudo firewall-cmd --reload
 sudo /usr/libexec/oci-growfs -y
 
 #podman and utensils - https://docs.oracle.com/en/operating-systems/oracle-linux/podman/podman-InstallingPodmanandRelatedUtilities.html
-sudo dnf install -y container-tools sqlcl jdk21 wget git podman-compose 
-# sudo dnf install -y oracle-epel-release-el9
-# sudo dnf config-manager --enable ol9_developer_EPEL
-
-
-
+sudo dnf install -y container-tools sqlcl jdk21 wget git podman-compose
 
 #git clone the compose sources to be added
 git clone https://github.com/klazarz/compose2cloud.git
@@ -43,7 +37,7 @@ chmod 777 compose2cloud/composescript/ords_config/
 chmod 777 compose2cloud/composescript/app/
 chmod 777 compose2cloud/composescript/model/
 
-podman-compose -f compose2cloud/composescript/compose.yml up 
+sudo cp compose2cloud/composescript/scripts/podman-compose.service /etc/systemd/system/.
 
-
-
+sudo systemctl daemon-reload
+sudo systemctl enable podman-compose.service
